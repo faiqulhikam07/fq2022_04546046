d42842642133c383a996a7233ce54179de6e7f0c
faiqulhikam07 <faiqulhikam7@gmail.com>
2022-11-11 00:15:58 +0700

    data masuk

--- kuliah/pertemuan2/functions.php
+++ kuliah/pertemuan2/functions.php
@@ -22,3 +22,22 @@ function query($query)
   }
   return $rows;
 }
+function tambah($data)
+{
+  $conn = koneksi();
+
+  $nama = $data['nama'];
+  $nim = $data['nim'];
+  $email = $data['email'];
+  $jurusan = $data['jurusan'];
+  $gambar = $data['gambar'];
+
+  $query = "INSERT INTO
+              mahasiswa
+            VALUES
+            (null, '$nama', '$nim', '$email', '$jurusan', '$gambar')
+          ";
+  mysqli_query($conn, $query);
+  echo mysqli_error($conn);
+  return mysqli_affected_rows($conn);
+}
---
+++ kuliah/pertemuan2/tambah.php
@@ -0,0 +1,68 @@
+<?php
+require 'functions.php';
+
+// cek apakah tombol tambah sudah ditekan
+if (isset($_POST['tambah'])) {
+  if (tambah($_POST) > 0) {
+    echo "<script>
+            alert('data berhasil ditambahkan');
+            document.location.href = 'latihan3.php';
+         </script>";
+  } else {
+    echo "data gagal ditambahkan!";
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tambah Data Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Form Tambah Data Mahasiswa</h3>
+  <form action="" method="POST">
+    <ul>
+      <li>
+        <label>
+          Nama :
+          <input type="text" name="nama" autofocus>
+        </label>
+      </li>
+      <li>
+        <label>
+          NIM :
+          <input type="text" name="nim">
+        </label>
+      </li>
+      <li>
+        <label>
+          Email :
+          <input type="text" name="email">
+        </label>
+      </li>
+      <li>
+        <label>
+          Jurusan :
+          <input type="text" name="jurusan">
+        </label>
+      </li>
+      <li>
+        <label>
+          Gambar :
+          <input type="text" name="gambar">
+        </label>
+      </li>
+      <li>
+        <button type="submit" name="tambah">Tambah Data</button>
+      </li>
+    </ul>
+  </form>
+</body>
+
+</html>
